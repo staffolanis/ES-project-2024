f140d9968e55b9812e9f8ca4b1310fce156eed78
Stefano Staffolani <10632170@mail.polimi.it>
2024-03-04 10:29:56 +0100

    test template

--- main.cpp
+++ main.cpp
@@ -6,49 +6,63 @@
 using namespace std;
 using namespace miosix;
 
-
 typedef Gpio<GPIOD_BASE, 12> greenLED; // Define GPIO pin for green LED
 
-// function to calculate PLL's parameters
-void setParameters(float in_f, float out_f_target) {
-    float usb_check = USB_FREQ;
-    int N = 0, M = 0, P = 0, Q = 0;
+struct Parameters {
+    int N;
+    int M;
+    int P;
+    int Q;
 
-    for (int n = 50; n <= 432; n++) {
-        for (int m = 2; m <= 63; m++) {
-            float vco = in_f * n / m;
-            for (int q = 2; q <= 15; q++) {
-                for (int p = 2; p<= 8; p+=2) {
+    int errorF;
+    int errorFUSB;
+
+    Parameters() : N(0), M(0), P(0), Q(0), errorF(0), errorFUSB(0) {}
+};
+
+template<int _InF, int _OutFTarget>
+Parameters setParameters() {
+    Parameters params;
+    constexpr float UsbCheck = (float)USB_FREQ;
+    constexpr float InF = (float)_InF;
+    constexpr float OutFTarget = (float)_OutFTarget;
+    float usb_check = UsbCheck;
+
+    for (int n = 50; n < 433; n++) {
+        for (int m = 2; m < 64; m++) {
+            float vco = InF * n / m;
+            for (int q = 2; q < 16; q++) {
+                for (int p = 2; p < 10; p += 2) {
                     float out_f = vco / p;
                     float out_usb = vco / q;
                     float diff_usb = (float)USB_FREQ - out_usb;
 
-                    if ((out_f == out_f_target) && (diff_usb <= usb_check && diff_usb >= 0)) {
+                    if ((out_f == OutFTarget) && (diff_usb <= usb_check && diff_usb >= 0.0)) {
                         usb_check = diff_usb;
-                        N = n;
-                        M = m;
-                        P = p;
-                        Q = q;
-                        iprintf("out_f=%d, usb_f=%d\nn:%d, m:%d, p:%d, q:%d\n", (int)out_f, (int)out_usb, N,M,P,Q);
+                        params.N = n;
+                        params.M = m;
+                        params.P = p;
+                        params.Q = q;
+
+                        //TODO: pipo -> check frequence error micro
+                        /*params.errorF = (int)(out_f-OutFTarget);
+                        if(params.errorF<0) params.errorF *= -1;
+                        params.errorF /= OutFTarget;*/
+                        params.errorFUSB = (int)((diff_usb/(float)USB_FREQ)*10000);
                     }
                 }
             }
         }
     }
-
-    // calculating values with the found configuration
-    float vco = in_f * N / M;
-    float out_f = vco / P;
-    float out_usb = vco / Q;
-
-    iprintf("Configurazione finale:\n n: %d\n m: %d\n p: %d\n q: %d\n", N,M,P,Q);
-    iprintf("output freq: %d\n", (int)out_f);
-    iprintf("usb freq: %d\n", (int)out_usb);
+    return params;
 }
 
 int main() {
     // debug parameters' calculation
-    setParameters(8000000.0, 168000000.0);
+    Parameters result = setParameters<8000000, 100000000>();
+    iprintf("Configurazione finale:\n n: %d\n m: %d\n p: %d\n q: %d\n", result.N ,result.M ,result.P ,result.Q );
+    iprintf("F error: %d/100\n", result.errorF);
+    iprintf("F_USB error: %d/100\n", result.errorFUSB);
 
     // blinking led as experiment
     greenLED::mode(Mode::OUTPUT);
